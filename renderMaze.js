2065c7c93c903234c46b6d68eeb7b45b1b17499d
rho <rho5ackiah@gmail.com>
2023-11-02 13:05:12 +0100

    ajout fichier index et renderMaze

---
+++ renderMaze.js
@@ -0,0 +1,25 @@
+let mazeWalls = []
+
+function renderMaze(ctx, mazeArray) {
+    for (let i = 0; i < mazeArray.length; i++) {
+        for (let j = 0; j < mazeArray[i].length; j++) {
+
+          let wall={}
+          ctx.beginPath();
+          ctx.rect(j * roadWidth, i * roadHeight, roadWidth, roadHeight)
+          ctx.fillStyle = mazeArray[i][j] === 0 ? 'white' : '#3581fc';
+          ctx.fill();
+          ctx.closePath()
+          
+        //Array to store position of walls 
+          if (mazeArray[i][j]==1){
+            wall.xcol=i;
+            wall.ycol=j;
+            wall.startXPos=j*roadWidth;
+            wall.startYPos=i*roadWidth;
+            mazeWalls.push(wall);
+        }
+        
+    }
+}
+ }
